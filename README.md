fd706cb5e0af115cab0b9a17197e59c5dfbb6060
BAS Eliot <eliot.bas@orange.fr>
2023-11-20 14:28:57 +0100

    seance 2 eliot

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Auteurs : BAS Eliot et ARNAUD-GODDET Etienne
 * Comment avez-vous choisi de programmer l'analyse de texte (dont la lecture
 des opérandes entières) ? Comment identifiez-vous les noms de registres, des noms "jolis", des entiers ?
 
-Pour l'analyse de texte, nous avons décidé d'utiliser la commande getline() qui nous retourne la figne entière. Nous avons ensuite remplacé les virgules et parenthèses par des espaces pour nettoyer l'instruction. Nous avons ensuite extrait l'instruction en la sauvgardant dans une variable. Ceci rendra plus simple la recherche de l'instruction parmis toutes celles implémentées.
+Pour l'analyse de texte, nous avons décidé d'utiliser la commande getline() qui nous retourne la figne entière. Nous avons ensuite remplacé les virgules et parenthèses par des espaces pour nettoyer l'instruction. Nous avons ensuite extrait l'instruction en la sauvgardant dans une variable. Ceci rendra plus simple la recherche de l'instruction parmis toutes celles implémentées. Pour identifier les noms de registres, des noms "jolis", des entiers, on teste si l'argument est un nom de registre, puis un entier, sinon c'est un nom "joli".
 
 
 * Avez-vous vu des motifs récurrents émerger ? Avez-vous "factorisé" ces motifs
--- assembler/main.c
+++ assembler/main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+int getInstructionFromLine(char *line, char instruction[5], char arguments[3][5]);
+int getRegisterWithAlias(char *alias);
+int stringToInt(char *string);
+
 int main(int argc, char **argv)
 {
     /* ./riscv-assembler <ASSEMBLER INPUT> <HEX OUTPUT> */
@@ -19,23 +23,11 @@ int main(int argc, char **argv)
     size_t len;
     char *line = NULL;
     while (getline(&line, &len, inputFile) != -1) {
-        printf("%s\n", line);
-        if (line[0] == '#' || line[0] == '\n') continue; // ignore les lignes vides et les commentaires
-        // nettoyage de la chaine
-        int i = 0;
-        while(line[i] != '\n') {
-            if (line[i] == ',') line[i] = ' ';
-            else if (line[i] == '(' || line[i] == ')') line[i] = ' ';
-            i++;
-        }
-        // extraction de l'instruction
-        char instruction[5];
-        int j = 0;
-        while (line[j] == ' ') {
-            j++;
-        }
-        strcpy(instruction, line + j); // nb caractères ?
-        printf("%s", instruction);
+        char instruction[10];
+        char arguments[3][5];
+        int args_counter = 0;
+        args_counter = getInstructionFromLine(line, instruction, arguments);
+        if (args_counter) printf("%s\n", instruction);
     }
 
     free(line);
@@ -44,4 +36,59 @@ int main(int argc, char **argv)
     fclose(outputFile);
 
     return 0;
+}
+
+int getInstructionFromLine(char *line, char instruction[5], char arguments[3][5]) {
+    if (line[0] == '#' || line[0] == '\n') return 0; // ignore les lignes vides et les commentaires
+    // nettoyage de la chaine
+    int i = 0;
+    while(line[i] != '\n') {
+        if (line[i] == ',') line[i] = ' ';
+        else if (line[i] == '(' || line[i] == ')') line[i] = ' ';
+        i++;
+    }
+
+    int j = 0;
+    // skip les premiers espaces
+    while (line[j] == ' ') j++;
+
+    // extraction de l'instruction
+    while (line[j] != ' ') {
+        instruction[j] = line[j];
+        j++;
+    }
+    instruction[j] = '\0';
+
+    // extraction des arguments
+    int arg_counter = 0;
+    while(line[j] != '\n' && arg_counter < 3) {
+        while (line[j] == ' ') j++; // skip les espaces
+        int l = 0;
+        while (line[j] != ' ' && line[j] != '\n') {
+            arguments[arg_counter][l] = line[j];
+            j++;
+            l++;
+        }
+        arguments[arg_counter][l] = '\0';
+        arg_counter++;
+    }
+    return arg_counter;
+}
+
+int getRegisterWithAlias(char *alias) {
+    char aliases[32][5] = {"zero", "ra", "sp", "gp","tp", "t0", "t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9" ,"s10" ,"s11", "t3", "t4", "t5", "t6"};
+    int register_number = 0;
+    while (strcmp(alias, aliases[register_number])) register_number++;
+    return register_number;
+}
+
+int stringToInt(char *string) {
+    int result = 0;
+    int i = 0;
+    while (string[i] != '\0') {
+        int c = string[i] - '0';
+        result = result * 10 + c;
+        i++;
+    }
+    return result;
 }
