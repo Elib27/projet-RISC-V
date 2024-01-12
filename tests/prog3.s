113ce418e12b0d26d7c8a4ac98b397befcd12164
etienne <etienne@qsdfghjklm.fr>
2024-01-12 14:26:58 +0100

    seance 6

--- tests/prog3.s
+++ tests/prog3.s
@@ -8,14 +8,16 @@ beq a0, t0, 20
 addi sp, sp, -8
 sd a0, 0(sp)
 addi a0, a0, 1
-j -12
+j -16
 #addi s0, zero, 1
 
 
 
 
+
+
 # EXPECTED
-# sp : 16384
-# a0 : 0
+# sp : 16304
+# a0 : 10
 # t0 : 10
 # a1 : 0
